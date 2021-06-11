a98bdce2bbbe6dc875e1bc2081c4daad5e11c4e3
Aditya Chandra <adityachandra9898@gmail.com>
2021-06-11 20:39:24 +0530

    week_3_solutions

---
+++ week3_greedy_algorithms/4_maximum_advertisement_revenue/maximum_advertisement_revenue.cpp
@@ -0,0 +1,28 @@
+//json_illusion
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+long long max_dot_product(vector<int> a, vector<int> b) {
+  // write your code here
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+  long long result = 0;
+  for (size_t i = 0; i < a.size(); i++) {
+    result += ((long long) a[i]) * b[i];
+  }
+  return result;
+}
+
+int main() {
+  size_t n;
+  std::cin >> n;
+  vector<int> a(n), b(n);
+  for (size_t i = 0; i < n; i++) {
+    std::cin >> a[i];
+  }
+  for (size_t i = 0; i < n; i++) {
+    std::cin >> b[i];
+  }
+  std::cout << max_dot_product(a, b) << std::endl;
+}
